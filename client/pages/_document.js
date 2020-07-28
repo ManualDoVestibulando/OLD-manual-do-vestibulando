e7085c217db544c61d6fb7cf216eb5a478154041
Eric Oliveira Gomes <ericog123456@gmail.com>
2020-07-28 14:56:21 -0300

    :sparkles: Add home page

--- client/pages/_document.js
+++ client/pages/_document.js
@@ -6,7 +6,7 @@ import theme from '../src/theme';
 export default class MyDocument extends Document {
   render() {
     return (
-      <Html lang="en">
+      <Html lang="pt-br">
         <Head>
           {/* PWA primary color */}
           <meta name="theme-color" content={theme.palette.primary.main} />
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,5 +1,30 @@
+import React, { Fragment } from 'react';
 import Head from 'next/head';
+import { Container, Typography } from '@material-ui/core';
+import Navbar from '../components/Navbar';
 
 export default function Home() {
-  return <div>Teste</div>;
+  return (
+    <Fragment>
+      <Navbar />
+      <div className="spacer"></div>
+      <Container maxWidth="sm">
+        <Typography variant="h3">Texto</Typography>
+        <Typography variant="body1">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+          blanditiis tenetur unde suscipit, quam beatae rerum inventore
+          consectetur, neque doloribus, cupiditate numquam dignissimos laborum
+          fugiat deleniti? Eum quasi quidem quibusdam.
+        </Typography>
+      </Container>
+
+      <style jsx>
+        {`
+          .spacer {
+            margin-top: 100px;
+          }
+        `}
+      </style>
+    </Fragment>
+  );
 }
